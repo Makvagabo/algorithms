5fe0b5cf27321cf81c75adecc17d344f655edd06
Alexander Petropavlovskikh <leizarlok@gmail.com>
2019-08-12 02:13:15 +0300

    add minimumSwaps

---
+++ src/Hackerrank/minimum_swaps.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Alexander on 11.08.2019.
+//
+
+#include <vector>
+
+#include "minimum_swaps.h"
+
+namespace Hackerrank {
+
+int minimumSwaps(std::vector<int> arr) {
+  int swaps = 0;
+  int size = arr.size();
+
+  return swaps;
+}
+
+}
---
+++ src/Hackerrank/minimum_swaps.h
@@ -0,0 +1,14 @@
+//
+// Created by Alexander on 11.08.2019.
+//
+
+#include <vector>
+
+#ifndef ALGORITHMS_HACKERRANK_MINIMUM_SWAPS_H_
+#define ALGORITHMS_HACKERRANK_MINIMUM_SWAPS_H_
+namespace Hackerrank {
+
+int minimumSwaps(std::vector<int> arr);
+
+}
+#endif //ALGORITHMS_HACKERRANK_MINIMUM_SWAPS_H_
